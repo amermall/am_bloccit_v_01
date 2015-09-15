652bee62f8a1d5ce300286c7348eeb3f3e2dab2c
Amer Mall <amer@icloud.com>
2015-09-14 22:53:39 -0400

    assignment_25_static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
+  get 'welcome/contact'
+
   # The "root" method allows us to declare the default page your app loads when we type the home page URL.
   root to: 'welcome#index'
 end
